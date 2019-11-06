71f34e762afbd508836018703fbceb69266ee1dc
Caleb Olson <calebols@hotmail.com>
2019-11-05 18:28:12 -0700

    changed color

--- homepage/templates/__dmpcache__/base.htm.py
+++ homepage/templates/__dmpcache__/base.htm.py
@@ -5,14 +5,14 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1572801587.2641041
+_modified_time = 1572921610.2116883
 _enable_loop = True
 _template_filename = 'C:/Users/Caleb/OneDrive - BYU Office 365/BYU/other/Python/EastlakeService/homepage/templates/base.htm'
 _template_uri = 'base.htm'
 _source_encoding = 'utf-8'
 import django_mako_plus
 import django.utils.html
-_exports = ['page_title', 'header_maintenance', 'site_left', 'site_center', 'site_right']
+_exports = ['page_title', 'header_maintenance', 'site_center']
 
 
 def render_body(context,**pageargs):
@@ -21,16 +21,12 @@ def render_body(context,**pageargs):
         __M_locals = __M_dict_builtin(pageargs=pageargs)
         def site_center():
             return render_site_center(context._locals(__M_locals))
-        def site_left():
-            return render_site_left(context._locals(__M_locals))
         request = context.get('request', UNDEFINED)
-        def site_right():
-            return render_site_right(context._locals(__M_locals))
-        def page_title():
-            return render_page_title(context._locals(__M_locals))
-        self = context.get('self', UNDEFINED)
         def header_maintenance():
             return render_header_maintenance(context._locals(__M_locals))
+        self = context.get('self', UNDEFINED)
+        def page_title():
+            return render_page_title(context._locals(__M_locals))
         STATIC_URL = context.get('STATIC_URL', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('<!DOCTYPE html>\r\n<html>\r\n    <meta charset="UTF-8">\r\n    <head>\r\n\r\n            <title>')
@@ -65,22 +61,12 @@ def render_body(context,**pageargs):
             __M_writer('                                    <li class="nav-item"><a class="nav-link-')
             __M_writer(django_mako_plus.ExpressionPostProcessor(self)( 'active' if request.dmp.page == 'records' else '' ))
             __M_writer('" href="/records/">My Service</a></li>\r\n')
-        __M_writer('                            </ul>\r\n                        </nav>\r\n                            \r\n                        </div>\r\n                       \r\n        </header>\r\n\r\n        <main>\r\n            <div id="site_left">\r\n                ')
-        if 'parent' not in context._data or not hasattr(context._data['parent'], 'site_left'):
-            context['self'].site_left(**pageargs)
-        
-
-        __M_writer('\r\n            </div>\r\n            <div id="site_center">\r\n                ')
+        __M_writer('                            </ul>\r\n                        </nav>\r\n                            \r\n                        </div>\r\n                       \r\n        </header>\r\n\r\n        <main>\r\n         \r\n            <div id="site_center">\r\n                ')
         if 'parent' not in context._data or not hasattr(context._data['parent'], 'site_center'):
             context['self'].site_center(**pageargs)
         
 
-        __M_writer('\r\n            </div>\r\n            <div id="site_right">\r\n                ')
-        if 'parent' not in context._data or not hasattr(context._data['parent'], 'site_right'):
-            context['self'].site_right(**pageargs)
-        
-
-        __M_writer('\r\n            </div>\r\n            \r\n        </main>\r\n\r\n        <footer class="page-footer">\r\n')
+        __M_writer('\r\n            </div>\r\n            \r\n            \r\n        </main>\r\n\r\n        <footer class="page-footer">\r\n')
         __M_writer('            &copy;Copyright 2019 -- Olson Web Design. All rights reserved.\r\n')
         __M_writer('        </footer>\r\n\r\n    </body>\r\n</html>')
         return ''
@@ -110,18 +96,6 @@ def render_header_maintenance(context,**pageargs):
         context.caller_stack._pop_frame()
 
 
-def render_site_left(context,**pageargs):
-    __M_caller = context.caller_stack._push_frame()
-    try:
-        def site_left():
-            return render_site_left(context)
-        __M_writer = context.writer()
-        __M_writer('\r\n                ')
-        return ''
-    finally:
-        context.caller_stack._pop_frame()
-
-
 def render_site_center(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
@@ -134,20 +108,8 @@ def render_site_center(context,**pageargs):
         context.caller_stack._pop_frame()
 
 
-def render_site_right(context,**pageargs):
-    __M_caller = context.caller_stack._push_frame()
-    try:
-        def site_right():
-            return render_site_right(context)
-        __M_writer = context.writer()
-        __M_writer('\r\n                ')
-        return ''
-    finally:
-        context.caller_stack._pop_frame()
-
-
 """
 __M_BEGIN_METADATA
-{"filename": "C:/Users/Caleb/OneDrive - BYU Office 365/BYU/other/Python/EastlakeService/homepage/templates/base.htm", "uri": "base.htm", "source_encoding": "utf-8", "line_map": {"18": 0, "36": 2, "41": 7, "42": 11, "43": 11, "44": 11, "45": 12, "46": 12, "47": 13, "48": 13, "49": 14, "50": 14, "51": 17, "52": 22, "53": 23, "54": 23, "59": 27, "60": 34, "61": 34, "62": 37, "63": 37, "64": 38, "65": 39, "66": 39, "67": 39, "68": 41, "73": 51, "78": 55, "83": 59, "84": 66, "85": 68, "91": 7, "102": 27, "113": 50, "119": 50, "125": 54, "131": 54, "137": 58, "143": 58, "149": 143}}
+{"filename": "C:/Users/Caleb/OneDrive - BYU Office 365/BYU/other/Python/EastlakeService/homepage/templates/base.htm", "uri": "base.htm", "source_encoding": "utf-8", "line_map": {"18": 0, "32": 2, "37": 7, "38": 11, "39": 11, "40": 11, "41": 12, "42": 12, "43": 13, "44": 13, "45": 14, "46": 14, "47": 17, "48": 22, "49": 23, "50": 23, "55": 27, "56": 34, "57": 34, "58": 37, "59": 37, "60": 38, "61": 39, "62": 39, "63": 39, "64": 41, "69": 52, "70": 60, "71": 62, "77": 7, "88": 27, "99": 51, "105": 51, "111": 105}}
 __M_END_METADATA
 """
